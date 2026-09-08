451ebbb5c5fa7c6a6e4eb2865442982d0b65c60f
Obalouch <abdolhakimarish@gmail.com>
2026-09-08 16:03:39 +0330

    sitemap config

--- solo-project/solo_project/solo_project/urls.py
+++ solo-project/solo_project/solo_project/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
+from django.contrib.sitemaps.views import sitemap
+from SoloApp.sitemaps import AnotherSitemap
+
+sitemaps = {
+    'posts' : AnotherSitemap
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('SoloApp.urls',namespace='solo'))
+    path('',include('SoloApp.urls',namespace='solo')),
+    path('sitemap.xml',sitemap,{'sitemaps':sitemaps},name='django.contrib.sitemaps.views.sitemap')
 ]
