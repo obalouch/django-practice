710b48a9045826644d61494df539b3b0ad4eb720
Obalouch <abdolhakimarish@gmail.com>
2026-09-08 15:43:30 +0330

    add markdown  filter

--- solo-project/solo_project/SoloApp/templatetags/solo_tags.py
+++ solo-project/solo_project/SoloApp/templatetags/solo_tags.py
@@ -1,7 +1,8 @@
 from django import template
 from ..models import Post
 from django.db.models import Count
-
+import markdown
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -18,3 +19,7 @@ def LatestPost(count=5):
 def mostComments(count=3):
     mostCP = Post.published.annotate(most_comment = Count('comments')).order_by('-most_comment')[:count]
     return mostCP
+
+@register.filter(name='markdown')
+def markdown_filter(text):
+    return mark_safe(markdown.markdown(text))
